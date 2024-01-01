a055e66bf543851b9209a923f06816444a1d7f0d
Frankie <frankidatank@gmail.com>
2024-01-01 14:11:07 -0700

    feat/add flag to analyze button to prevent spamming

--- ui/critic_window.go
+++ ui/critic_window.go
@@ -7,13 +7,14 @@ import (
 )
 
 type CriticWindow struct {
-	Size                fyne.Size // The size of the application window
-	App                 *fyne.App
-	Canvas              *fyne.Container                 // A box containing the ui components
-	Window              *fyne.Window                    // The main application window
-	ToolBar             *fyne.CanvasObject              // the top toolbar menu
-	ReportPanel         *components.ReportPanel         // the left panel
-	DiffPanel           *components.DiffPanel           // The right panel
-	ProgressBar         *components.ProgressBar         // The progress bar
-	PullRequestURLModal *components.PullRequestURLModal // The API key modal
+	Size                  fyne.Size // The size of the application window
+	App                   *fyne.App
+	Canvas                *fyne.Container                 // A box containing the ui components
+	Window                *fyne.Window                    // The main application window
+	ToolBar               *fyne.CanvasObject              // the top toolbar menu
+	ReportPanel           *components.ReportPanel         // the left panel
+	DiffPanel             *components.DiffPanel           // The right panel
+	ProgressBar           *components.ProgressBar         // The progress bar
+	PullRequestURLModal   *components.PullRequestURLModal // The API key modal
+	IsAnalyzeButtonActive bool                            // flag to sprevent spamming of analyze button
 }
